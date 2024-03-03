e0ca159e0eeaac32e0691f4af7ef63e239bf40d7
wefa <YSX13477013297@outlook.com>
2024-03-03 10:10:24 +0800

    homepagefrontchange

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,68 @@
 <template>
   <div class="查重系统">
-    <title>查重系统</title>
-    <p>查重系统</p>
+    <div class="header">
+      <div class="logo">
+        <img class="bjtulogo" src="./assets/bjjtLogo.png" alt="logo"></img>
+      </div>
+    </div>
+    <div id="split"></div>
 <!-- 使用 router-link 添加按钮跳转至 /submit -->
 <!--使用 router-link 组件进行导航 -->
 <!--通过传递 `to` 来指定链接 -->
 <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
-    <router-link to="/">Home</router-link>
-    <router-link to="/Submit">  submit</router-link>
+    <router-link to="/"><button class="home">首页</button></router-link>
+    <router-link to="/Submit"><button class="chachong">查重</button></router-link>
     <router-view></router-view>
   </div>
 </template>
 <style scoped>
-.logo {
-  height: 6em;
+.查重系统 {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #f3f3f3;
+}
+
+.header {
+  width: 100%; /* 使用百分比单位，100% 表示整个页面宽度 */
+  margin:0;
+  max-width:none;
+  overflow: hidden;
+  height: 101.68px;
+  background-color: #f3f3f3;
+}
+#split{
+  width: 100%;
+  height: 1px; /* 设置分界线高度 */
+  background-color: #075fae; /* 设置分界线颜色 */
+  content: "";
+  display: block;
+  margin-top: 0px; /* 设置分界线与上方元素的间距，可根据需求调整 */
+}
+.bjtulogo {
+  height: 65px;
+  width: 20%;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
+  margin-left: 30px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.home{
+  border:none;
+  outline: none;
+  width: 15%; 
+  height: 40px;
+  color: #075fae;
+  font-size: 20px;
+  font-weight: bold;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.chachong{
+  border:none;
+  outline: none;
+  width: 15%; 
+  height: 40px;
+  color: #075fae;
+  font-size: 20px;
+  font-weight: bold;
 }
 </style>
